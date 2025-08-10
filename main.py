5308b2c3b3b061645cb21a44907c0a2453064d84
joaohnp <joaohnp@gmail.com>
2025-08-10 22:13:40 +0200

    adding logic to send telegram message indicating the bot is up and running

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import time
+from datetime import datetime
 from sys import exit
 
+from telegram_utils import send_telegram_message
 from utils import get_trades, initialize_price
 
 highest_price_reference = initialize_price()
 
 if highest_price_reference is None:
     exit()
-
+last_sent_minute_block = -1
 while True:
+    current_minute_block = datetime.datetime.now().minute // 15
+    if current_minute_block != last_sent_minute_block:
+        send_telegram_message("Bot up and running")
+        last_sent_minute_block = current_minute_block
     try:
         highest_price_reference = get_trades(highest_price_reference)
         time.sleep(10)
